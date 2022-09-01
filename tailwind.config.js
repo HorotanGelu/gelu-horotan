c08f9e753b8249014354234f958b1d102a48783d
Horotan Gelu <horotangelu17@gmail.com>
2022-09-01 16:39:33 +0300

    Few tests for the video element on landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./pages/*.{html,js,jsx,tsx}', './components/*/.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: '#9356A0',
+        light: '#000',
+      },
+    },
     extend: {},
   },
   plugins: [],
