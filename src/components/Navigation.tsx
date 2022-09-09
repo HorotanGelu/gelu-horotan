969f6f9cd9a6b2915f89494bdafa7e01002751ba
Horotan Gelu <horotangelu17@gmail.com>
2022-09-10 00:44:03 +0300

    useDarkMode hook has been partially implemented

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
 import * as FaIcons from 'react-icons/fa'
+import useDarkMode from '../customHooks/useDarkMode'
 
 const Navigation = () => {
+  const [darkTheme, setDarkTheme] = useDarkMode()
+  const handleMode = () => setDarkTheme(!darkTheme)
   const navItems = [
     { id: 1, name: 'home' },
-    { id: 1, name: 'about' },
-    { id: 1, name: 'projects' },
-    { id: 1, name: 'contact' },
-    { id: 1, name: 'login' },
-    { id: 1, name: 'register' },
+    { id: 2, name: 'about' },
+    { id: 3, name: 'projects' },
+    { id: 4, name: 'contact' },
+    { id: 5, name: 'login' },
+    { id: 6, name: 'register' },
   ]
   return (
-    <nav className='flex flex-row w-full py-3 px-2  justify-between bg-slate-600 items-center'>
+    <nav className='flex flex-row w-full py-3 px-2  justify-between dark:bg-gray-900 dark:text-white  items-center'>
       <FaIcons.FaApplePay size='28' />
       <ul className='flex flex-row w-1/2 justify-around items-center'>
         {navItems?.map(item => {
           return (
             <li key={item.id} className='navigation-item'>
-              <Link href={`/${item.name === 'home' ? '/' : item.name}`}>
+              <Link href={`/${item.name === 'home' ? '' : item.name}`}>
                 {item.name}
               </Link>
             </li>
           )
         })}
       </ul>
+      <span onClick={handleMode}>
+        {darkTheme ? (
+          <FaIcons.FaSun size='24' className='top-navigation-icon' />
+        ) : (
+          <FaIcons.FaMoon size='24' className='top-navigation-icon' />
+        )}
+      </span>
     </nav>
   )
 }
