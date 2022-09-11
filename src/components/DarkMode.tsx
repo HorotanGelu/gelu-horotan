4c0901414c3f40050aa194ad602c44f01ba70a85
Horotan Gelu <horotangelu17@gmail.com>
2022-09-11 19:03:40 +0300

    Dark mode colors reworked

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -6,8 +6,13 @@ const DarkMode = () => {
   const [darkTheme, setDarkTheme] = useDarkMode()
   const handleMode = () => setDarkTheme(!darkTheme)
   return (
-    <span onClick={handleMode} className='absolute py-5 px-2 text-purple-700'>
-      {darkTheme ? <FaIcons.FaSun size='24' /> : <FaIcons.FaMoon size='24' />}
+    <span
+      onClick={handleMode}
+      className={`absolute py-5 px-2  ${
+        darkTheme ? 'text-primary' : 'text-yellow-400'
+      }`}
+    >
+      {darkTheme ? <FaIcons.FaMoon size='24' /> : <FaIcons.FaSun size='24' />}
     </span>
   )
 }
