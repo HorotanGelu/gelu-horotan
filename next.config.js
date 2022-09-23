a01962db15c756668e6aef1beeaeed5f2d92e4b1
Horotan Gelu <horotangelu17@gmail.com>
2022-09-24 02:23:37 +0300

    Removing next loaders

--- next.config.js
+++ next.config.js
@@ -3,20 +3,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  loaders: [
-    {
-      test: /\.html$/,
-      loader: 'html-loader?attrs[]=video:src',
-    },
-    {
-      test: /\.mp4$/,
-      loader: 'url?limit=10000&mimetype=video/mp4',
-    },
-    {
-      test: /\.webm$/,
-      loader: 'url?limit=10000&mimetype=video/webm',
-    },
-  ],
+  // loaders: [
+  //   {
+  //     test: /\.html$/,
+  //     loader: 'html-loader?attrs[]=video:src',
+  //   },
+  //   {
+  //     test: /\.mp4$/,
+  //     loader: 'url?limit=10000&mimetype=video/mp4',
+  //   },
+  //   {
+  //     test: /\.webm$/,
+  //     loader: 'url?limit=10000&mimetype=video/webm',
+  //   },
+  // ],
 }
 
 module.exports = nextConfig
