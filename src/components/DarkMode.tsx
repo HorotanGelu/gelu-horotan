c7daa00e3f5cb425682e186865a1ffdd7acb35d3
HorotanGelu <horotangelu17@gmail.com>
2022-11-08 13:46:48 +0200

    useMount hook created

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,51 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import useDarkMode from '../customHooks/useDarkMode'
+import useMount from '../customHooks/useMount'
 import { motion } from 'framer-motion'
 import { HiOutlineLightBulb, HiOutlineMoon } from 'react-icons/hi'
 
-const DarkMode: React.FC = props => {
+const DarkMode: React.FC = () => {
    const [colorTheme, setTheme] = useDarkMode()
-   const [mounted, setMounted] = useState<boolean>()
+   const [isMounted] = useMount()
 
-   useEffect(() => {
-      setMounted(true)
-   }, [])
-
-   return (
-      <div className='flex items-center gap-4'>
-         <HiOutlineLightBulb
-            className='text-secondary dark:text-primary'
-            size={24}
-         />
-         <span
-            className={`w-12 h-6 bg-secondary_s_2 rounded-full  p-1 cursor-pointer flex items-center ${
-               colorTheme === 'light' && mounted
-                  ? 'justify-start'
-                  : colorTheme === 'dark' && mounted
-                  ? 'justify-end'
-                  : ''
-            }  `}
-            onClick={() =>
-               colorTheme === 'light' ? setTheme('light') : setTheme('dark')
-            }
-         >
-            <motion.span
-               layout
-               transition={{
-                  type: 'spring',
-                  stiffness: 700,
-                  damping: 30,
-               }}
-               className='rounded-full w-4 h-4 bg-secondary_t_2 '
+   if (isMounted)
+      return (
+         <div className='flex items-center gap-4'>
+            <HiOutlineLightBulb
+               className='text-secondary dark:text-primary'
+               size={24}
             />
-         </span>
+            <div
+               className={`w-12 h-6 bg-secondary_s_2 rounded-full  p-1 cursor-pointer flex items-center ${
+                  colorTheme === 'light' && isMounted
+                     ? 'justify-start'
+                     : colorTheme === 'dark' && isMounted
+                     ? 'justify-end'
+                     : ''
+               }  `}
+               onClick={() =>
+                  colorTheme === 'light' ? setTheme('light') : setTheme('dark')
+               }
+            >
+               <motion.span
+                  layout
+                  transition={{
+                     type: 'spring',
+                     stiffness: 700,
+                     damping: 30,
+                  }}
+                  className='rounded-full w-4 h-4 bg-secondary_t_2 '
+               />
+            </div>
 
-         <HiOutlineMoon
-            className='text-secondary dark:text-primary'
-            size={24}
-         />
-      </div>
-   )
+            <HiOutlineMoon
+               className='text-secondary dark:text-primary'
+               size={24}
+            />
+         </div>
+      )
 }
 
 export default DarkMode
---
+++ src/customHooks/useMount.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+
+function useMount() {
+   const [isMounted, setIsMounted] = useState<boolean>(false)
+
+   useEffect(() => {
+      setIsMounted(true)
+   }, [isMounted])
+
+   return [isMounted]
+}
+
+export default useMount
